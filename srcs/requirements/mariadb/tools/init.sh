8abdf487b2f304aaf415d1eb0efda3c28955ff87
tkubanyc <tkubanyc@student.42heilbronn.de>
2025-02-23 13:19:15 -0500

    chore: Modify Dockerfile for MariaDB - replace base image for debian (alpine didn't work and crashed all the time during the process of creation)

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-# Initialize the database
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
+# Initialize the database if it doesn't exist
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	echo "Initializing MariaDB database..."
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
 
 # Start MariaDB temporarily to run SQL commands
-mysql_safe --user=mysql --datadir=/var/lib/mysql &
+mysqld_safe --user=mysql --datadir=/var/lib/mysql &
 sleep 5
 
 # Create the database and user
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE;"
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USER''@''%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVELEGES;"
+mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 # Stop the temporary MariaDB instance
 mysqladmin -uroot -p"$MYSQL_ROOT_PASSWORD" shutdown
